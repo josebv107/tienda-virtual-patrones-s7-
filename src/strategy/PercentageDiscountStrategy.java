846e177fb162389d7f612678550bdabe3e7ea011
Jose Bocanegra <josebv107@gmail.com>
2026-05-23 17:05:43 -0500

    feat: implementar estrategia de descuento porcentual

--- src/strategy/PercentageDiscountStrategy.java
+++ src/strategy/PercentageDiscountStrategy.java
@@ -1,4 +1,16 @@
 package strategy;
 
-public class PercentageDiscountStrategy {
+public class PercentageDiscountStrategy implements DiscountStrategy {
+    private double percentage;
+
+    public PercentageDiscountStrategy(double percentage) {
+        this.percentage = percentage;
+    }
+
+    @Override
+    public double applyDiscount(double total) {
+        double discount = total * (percentage / 100);
+        System.out.println("Descuento aplicado: " + percentage + "% = S/ " + discount);
+        return total - discount;
+    }
 }
