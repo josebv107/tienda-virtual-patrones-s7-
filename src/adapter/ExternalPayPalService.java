ff9a2c97f53c69f8ae5c8d70fe13b9da77a53f8c
Jose Bocanegra <josebv107@gmail.com>
2026-05-23 17:22:31 -0500

    docs: agregar comentarios explicativos a la clase ExternalPayPalService

--- src/adapter/ExternalPayPalService.java
+++ src/adapter/ExternalPayPalService.java
@@ -1,5 +1,6 @@
 package adapter;
 
+//Simula una librería de terceros, tiene una firma incompatible con PaymentProcessor
 public class ExternalPayPalService {
     public void makePayment(String currency, double amount) {
         System.out.println("Pago realizado con Paypal: " + currency + " " + amount);
