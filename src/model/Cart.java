0fd8aeb2046e80bce569a1046d657d1e2b2e0ab2
Jose Bocanegra <josebv107@gmail.com>
2026-05-23 16:49:39 -0500

    feat: convertir estrategia de descuento a interfaz e implementar estrategia sin descuento

--- src/model/Cart.java
+++ src/model/Cart.java
@@ -1,4 +1,18 @@
 package model;
 
+import strategy.NoDiscountStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
+    private List<Product> products;
+    private DiscountStrategy discountStrategy;
+
+    public Cart() {
+        this.products = new ArrayList<>();
+        this.discountStrategy = new NoDiscountStrategy();
+    }
+
+
 }
