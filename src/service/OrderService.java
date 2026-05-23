4b2cd2634af45bf4d0a9a28755bc659286be33bf
Jose Bocanegra <josebv107@gmail.com>
2026-05-23 17:48:07 -0500

    feat: implementar servicio de pedidos

--- src/service/OrderService.java
+++ src/service/OrderService.java
@@ -1,4 +1,47 @@
 package service;
 
+import adapter.PaymentProcessor;
+import model.Cart;
+import observer.OrderObserver;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderService {
+    private Cart cart;
+    private PaymentProcessor paymentProcessor;
+
+    private List<OrderObserver> observers;
+
+    public OrderService(Cart cart) {
+        this.cart = cart;
+        this.observers = new ArrayList<>();
+    }
+
+    public void setPaymentProcessor(PaymentProcessor paymentProcessor) {
+        this.paymentProcessor = paymentProcessor;
+    }
+
+    public void addObserver(OrderObserver observer) {
+        observers.add(observer);
+    }
+
+    public void confirmOrder() {
+        double total = cart.calculateTotal();
+
+        System.out.println("\n--- Confirmando pedido ---");
+        paymentProcessor.pay(total);
+
+        String message = "Compra confirmada por S/ " + total;
+        System.out.println(message);
+
+        notifyObservers(message);
+    }
+
+    private void notifyObservers(String message) {
+        System.out.println("\n--- Notificaciones ---");
+        for (OrderObserver observer : observers) {
+            observer.update(message);
+        }
+    }
 }
