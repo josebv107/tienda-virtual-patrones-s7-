55c7f76278f6103497f639c1357619bb2951374c
Jose Bocanegra <josebv107@gmail.com>
2026-05-23 17:08:33 -0500

    feat: implementar estrategia de descuento fijo

--- src/strategy/FixedAmountDiscountStrategy.java
+++ src/strategy/FixedAmountDiscountStrategy.java
@@ -1,4 +1,16 @@
 package strategy;
 
-public class FixedAmountDiscountStrategy {
+public class FixedAmountDiscountStrategy implements DiscountStrategy {
+    private double fixedAmount;
+
+    public FixedAmountDiscountStrategy(double fixedAmount) {
+        this.fixedAmount = fixedAmount;
+    }
+
+    @Override
+    public double applyDiscount(double total) {
+        double result = Math.max(0, total - fixedAmount);
+        System.out.println("Descuento fijo aplicado: S/ " + fixedAmount);
+        return result;
+    }
 }
