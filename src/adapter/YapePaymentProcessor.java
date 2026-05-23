c6dd136aeb5d60a994825cc8b3a865d168e8f0cb
Jose Bocanegra <josebv107@gmail.com>
2026-05-23 17:31:44 -0500

    feat: implementar procesador de pago con Yape

---
+++ src/adapter/YapePaymentProcessor.java
@@ -0,0 +1,9 @@
+package adapter;
+
+public class YapePaymentProcessor implements PaymentProcessor {
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Pago realizado con Yape: S/" + amount);
+    }
+}
